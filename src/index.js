6787f765493bba6e3f3139858412896729bee2c3
22B01A4262 AI & ML <22b01a4262@svecw.edu.in>
2024-07-23 00:03:06 +0530

    TestCommit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import ReactDOM from "react-dom/client";
-import GetReview from './Reviews';
+import Multiple from "./Multiple";
+// import GetReview from './Reviews';
+// import Profile from "./Profiles";
 const rootElement = document.getElementById('root')
 const rootRef = ReactDOM.createRoot(rootElement);
-rootRef.render(<GetReview />);
+rootRef.render(<Multiple />);
